b585a546d17139498b0e056debb825ca252546c6
David Eastmond <davebuck1981@gmail.com>
2022-05-06 17:10:01 -0400

    question stream updates to include product selection

--- src/data/insurance/question-stream/question-stream-data.ts
+++ src/data/insurance/question-stream/question-stream-data.ts
@@ -1,14 +1,22 @@
+import { insuranceProducts } from "../insurance-packages";
 import { TQuestion } from "./definitions";
 
 export const questionStream: TQuestion[] = [
   {
     id: "000",
+    type: "productSelection",
+    name: "productSelection",
+    label: "Select an insurance package",
+    products: insuranceProducts,
+  },
+  {
+    id: "001",
     name: "email",
     label: "What is your e-mail?",
     type: "text",
   },
   {
-    id: "001",
+    id: "002",
     name: "age",
     label: "What is your age?",
     min: 18,
@@ -16,7 +24,7 @@ export const questionStream: TQuestion[] = [
     type: "numberInput",
   },
   {
-    id: "002",
+    id: "003",
     name: "gender",
     label: "What is your gender?",
     type: "radio",
