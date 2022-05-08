99bbbaedc3bfc31875395401399d24c01494015f
David Eastmond <davebuck1981@gmail.com>
2022-05-08 17:38:02 -0400

    update main app to load question stream

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Stage } from "./components/stage";
+import { questionStream } from "./data/insurance/question-stream/question-stream-data";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Stage stream={questionStream} />
+    </div>
+  );
 }
 
 export default App;
